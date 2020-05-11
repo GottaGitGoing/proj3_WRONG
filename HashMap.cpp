9646b37f0befe867af9bc58b07bddff92cee8ce0
Arian <aryannamavar@gmail.com>
2020-05-10 17:32:20 -0700

    First Up

---
+++ HashMap.cpp
@@ -0,0 +1,18 @@
+// HashMap.cpp
+#include <string>
+#include "HashMap.hpp"
+
+
+unsigned int ascii_hash(const std::string& s)
+{
+    int hash_value = 0;
+    // Credit to Professor Pattis. I had the idea of changing letter to ascii and add
+    // the hash value. and I saw that professor pattis had done something similiar.
+    for (int i=0; i<s.size(); ++i)
+    {
+        hash_value = 31*hash_value + s[i]; 
+    }
+    return hash_value;
+}
+
+HashMap::HashFunction hash_it = ascii_hash;
